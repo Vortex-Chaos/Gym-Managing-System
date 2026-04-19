ce047de057178cc3ddb4dbbf6b175c5fafce6897
Vortex-Chaos <115407506+Vortex-Chaos@users.noreply.github.com>
2026-04-19 16:25:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+bool auth(); void showmenu(); 
+
+
+int main() {
+    if (!auth()) {
+        return 0;
+    }
+
+    int user_input;
+
+    // variable used to measure true size of the data
+    int size = 0;
+
+    const int GYM_SIZE = 10;
+
+    int member_id[GYM_SIZE];
+    string member_name[GYM_SIZE];
+    
+
+
+    do {
+        showmenu();
+        cin >> user_input;
+        switch (user_input) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                cout << "Existing...";
+                break;
+            default:
+                cout << "Invalid input, please enter a number from 1-7";
+                break;
+        }
+    } while (user_input != 7);
+}
+
+// Ensures correct password has been entered before starting the system.
+bool auth() {
+    string system_password;
+    string user_password;
+    fstream passfile;
+    passfile.open("password.txt", ios::in);
+    if (!passfile) {
+        cout << "System has no password yet, can't continue.";
+        passfile.close();
+        return false;
+    }
+    cout << "Enter password to access the system: ";
+    getline(cin, user_password);
+    getline(passfile, system_password);
+    if (user_password == system_password) {
+        passfile.close();
+        return true;
+    }
+    else {
+        cout << "Incorrect password.";
+        passfile.close();
+        return false;
+    }
+}
+
+void showmenu() {
+
+}
